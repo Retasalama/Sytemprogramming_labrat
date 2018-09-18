df0d4683751f0ad12e6f010df330061f3a34c366
Tanja <tanjaremes82@gmail.com>
2018-09-18 10:26:58 +0300

    LAB3 kakkonen tehty

--- LAB3/DataFetchExample/app/src/main/java/t/e/datafetchexample/MainActivity.java
+++ LAB3/DataFetchExample/app/src/main/java/t/e/datafetchexample/MainActivity.java
@@ -2,10 +2,11 @@ package t.e.datafetchexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NetworkLoaderThread.ResultInterface {
 
     private TextView resultView;
     private EditText giveUrl;
@@ -14,8 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultView = (TextView)findViewById(R.id.tv_results);
-        giveUrl = (EditText)findViewById(R.id.et_giveUrl);
+        resultView = (TextView) findViewById(R.id.tv_results);
+        giveUrl = (EditText) findViewById(R.id.et_giveUrl);
+
+    }
+
+    @Override
+    public void threadProgress(final String data) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                resultView.setText(data);
+            }
+        });
+
+    }
+
+    public void sendUrl(View view) {
+        String Url = giveUrl.getText().toString();
+        NetworkLoaderThread networkLoaderThread = new NetworkLoaderThread(Url);
+        networkLoaderThread.listener = this;
+        networkLoaderThread.start();
 
     }
 }
