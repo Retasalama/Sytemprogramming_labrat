f69862e8e688bb2a07bcd5b923018fcf2c6a52ca
Tanja <tanjaremes82@gmail.com>
2018-09-18 09:44:04 +0300

    Lab3 eka valmis

--- LAB3/DataFetchExample.java
+++ LAB3/DataFetchExample.java
@@ -1,2 +1,24 @@
 
-public
+import java.util.Scanner;
+
+public class DataFetchExample implements NetworkLoaderThread.ResultInterface {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    String URL = "";
+    System.out.println("Give URL: ");
+    URL = scanner.nextLine();
+    if (!URL.equals("")) {
+      NetworkLoaderThread networkLoaderThread = new NetworkLoaderThread(URL);
+      DataFetchExample dfe = new DataFetchExample();
+      networkLoaderThread.setListener(dfe);
+      networkLoaderThread.start();
+    }
+  }
+
+  @Override
+  public void threadProgress(String data){
+    System.out.println(data);
+  }
+}
