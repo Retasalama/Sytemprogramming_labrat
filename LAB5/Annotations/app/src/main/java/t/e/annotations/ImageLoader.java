4daf8faab638d5b0ba5691c6ca94a2e9454c12fb
Tanja <tanjaremes82@gmail.com>
2018-10-03 13:29:29 +0300

    LAB5.3 valmis

--- LAB5/Annotations/app/src/main/java/t/e/annotations/ImageLoader.java
+++ LAB5/Annotations/app/src/main/java/t/e/annotations/ImageLoader.java
@@ -1,8 +1,18 @@
 package t.e.annotations;
 
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -11,15 +21,58 @@ public class ImageLoader {
 
     ArrayList<Bitmap> bitmapArray;
     String[] urls;
+    Bitmap bmImg;
+    URL photoUrl;
+    int progress = 20;
+    protected ImageLoaderInterface listener = null;
 
     public ImageLoader(String[] aurls) {
         this.urls = aurls;
     }
 
+    protected interface ImageLoaderInterface {
+        void setImages(ArrayList<Bitmap> bitmapArray);
+        void showProgress(int progress);
+    }
+    public void setListener(ImageLoaderInterface aListener) {
+        this.listener = aListener;
+    }
+
 
     public void loadImages(){
         bitmapArray = new ArrayList<Bitmap>();
+        photoUrl =null;
+        for(int i = 0 ; i < urls.length ; i++) {
+            try {
+                photoUrl= new URL(urls[i]);
+                Log.d("URL", "Haettu url on " + photoUrl);
 
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                HttpURLConnection connection = (HttpURLConnection)photoUrl.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                bmImg = BitmapFactory.decodeStream(input);
+                bitmapArray.add(bmImg);
+                Log.d("BITMAP", "bitmaparray size on " + bitmapArray.size());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(listener != null) {
+                listener.showProgress(progress);
+                progress = progress + 20;
+            }
+
+        }
+
+        if(listener != null){
+            listener.setImages(bitmapArray);
+        }
 
     }
 }
--- LAB5/Annotations/app/src/main/java/t/e/annotations/MainActivity.java
+++ LAB5/Annotations/app/src/main/java/t/e/annotations/MainActivity.java
@@ -1,40 +1,110 @@
 package t.e.annotations;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @EActivity
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ImageLoader.ImageLoaderInterface {
+
+    private ArrayList<Bitmap> mushroomImages;
+    private String[] mushroomNames;
+    private String[] urls;
+    private ImageLoader myImageLoader;
+    private int clicks = 0;
 
     @ViewById(R.id.myUrlImage)
     ImageView mushroomImage;
 
-    @Background
-    public void getImages() {
-        Resources res = getResources();
-        String[] urls = res.getStringArray(R.array.urls);
-        ImageLoader myImageLoader = new ImageLoader(urls);
-        myImageLoader.loadImages();
+    @ViewById(R.id.tvImageText)
+    TextView mushroomName;
 
+    @ViewById(R.id.buttonNext)
+    Button buttonNext;
+
+    @ViewById(R.id.determinateBar)
+    ProgressBar progressBar;
+
+    @Click
+    void buttonNext() {
+        if(clicks < mushroomNames.length && clicks < mushroomImages.size()) {
+            buttonNext.setText(R.string.buttonNext);
+            mushroomName.setText(mushroomNames[clicks]);
+            mushroomImage.setImageBitmap(mushroomImages.get(clicks));
+            clicks++;
+        }
+        if(clicks == mushroomNames.length){
+            buttonNext.setText(R.string.startAgain);
+            clicks = 0;
+
+        }
     }
 
-    @UiThread
-    public void showImages() {
-
+    @Background
+    public void getImages() {
+        myImageLoader.loadImages();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mushroomImages = new ArrayList<Bitmap>();
+
+        //set the next-button invisible (waiting for imageloading)
+        buttonNext.setVisibility(View.GONE);
+
+        //get the url- and mushroomNames -arrays from resources
+        Resources res = getResources();
+        urls = res.getStringArray(R.array.urls);
+        mushroomNames = res.getStringArray(R.array.mushroomNames);
+
+        //create object from ImageLoader class and set listener
+        myImageLoader = new ImageLoader(urls);
+        myImageLoader.setListener(this);
         getImages();
+
+
     }
 
+    // set mushroom name to texview and image to imageviw from index of value "cliks"
+    @Override
+    @UiThread
+    public void setImages(ArrayList<Bitmap> bitmapArray) {
+        mushroomImages = bitmapArray;
+        mushroomName.setText(mushroomNames[clicks]);
+        mushroomImage.setImageBitmap(mushroomImages.get(clicks));
+        clicks++;
+
+    }
+
+    //shows progress and sets button visible when loading is done
+    @Override
+    @UiThread
+    public void showProgress(int progress) {
+        progressBar.setProgress(progress);
+        if(progress >= 100) {
+            progressBar.setVisibility(View.GONE);
+            buttonNext.setVisibility(View.VISIBLE);
+        }
+
+    }
 }
